88d1c67f417d525f49dd0851f9bd078f3241d1f2
Harsh860 <harshvardhan860@gmail.com>
2018-03-08 02:52:53 +0530

    added routing

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,14 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+     url(r'^product$', views.product, name='product'),
+      url(r'^career$', views.career, name='career'),
+       url(r'^contactus$', views.contactus, name='contactus'),
+        url(r'^aboutus$', views.aboutus, name='aboutus'),
+         url(r'^news$', views.news, name='news'),
+         url(r'^edu$', views.edu, name='edu'),
+         url(r'^distributor$', views.distributor, name='distributor'),
+         url(r'^business$', views.business, name='business'),
+         url(r'^investor$', views.investor, name='investor'),
+         url(r'^newsroom$', views.newsroom, name='newsroom'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,40 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+
+def product(request):
+    return render(request, 'blog/product.html', {})
+
+
+def career(request):
+    return render(request, 'blog/career.html', {})
+
+
+
+def contactus(request):
+    return render(request, 'blog/contactus.html', {})
+
+
+def edu(request):
+    return render(request, 'blog/edu.html', {})
+
+def aboutus(request):
+    return render(request, 'blog/aboutus.html', {})
+
+
+def news(request):
+    return render(request, 'blog/news.html', {})
+
+def distributor(request):
+    return render(request, 'blog/distributor.html', {})    
+
+def business(request):
+    return render(request, 'blog/business.html', {}) 
+
+def investor(request):
+    return render(request, 'blog/investor.html', {}) 
+
+
+def newsroom(request):
+    return render(request, 'blog/newsroom.html', {})     
